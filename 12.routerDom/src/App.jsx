128d9ac05005d57472e77f1fa4a89a3e50998db7
Ersin Uluağaç <ersinuluagac@gmail.com>
2026-04-08 20:06:19 +0300

    add react router dom

---
+++ 12.routerDom/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import Contact from "./pages/Contact";
+import NotFoundPage from "./pages/NotFoundPage";
+import EmployeeAbout from "./pages/EmployeeAbout";
+import CompanyAbout from "./pages/CompanyAbout";
+import Header from "./components/Header";
+import ProductDetails from "./pages/ProductDetails";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />}>
+          <Route path="employee" element={<EmployeeAbout />} />
+          <Route path="company" element={<CompanyAbout />} />
+        </Route>
+        <Route path="/products" element={<Products />} />
+        <Route path="/product-details/:id" element={<ProductDetails />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ 12.routerDom/src/pages/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <hr />
+      <Link to="employee">Çalışanlar</Link>
+      <Link to="company">Şirket</Link>
+      <Outlet />
+    </div>
+  );
+}
+
+export default About;
